5bcbef2bb67fe1b40b76a2371967a46b3eb48c61
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-03-26 14:47:55 +0530

    Handled the category adding part

--- src/main/java/edu/kolitha/solo/eCom/store/controller/CategoryController.java
+++ src/main/java/edu/kolitha/solo/eCom/store/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package edu.kolitha.solo.eCom.store.controller;
 
+import edu.kolitha.solo.eCom.store.model.Category;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,15 +9,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/shop/category")
 public class CategoryController {
-    private List<String> categoryList = new ArrayList<>();
+    private List<Category> categoryList = new ArrayList<>();
 
     @GetMapping("/all")
-    public List<String> getCategoryList(){
+    public List<Category> getCategoryList(){
         return categoryList;
     }
 
     @PostMapping("/add")
-    public String addCategory(@RequestBody String categoryName){
+    public String addCategory(@RequestBody Category categoryName){
+        categoryList.add(categoryName);
         return "Category added successfully!";
     }
 
