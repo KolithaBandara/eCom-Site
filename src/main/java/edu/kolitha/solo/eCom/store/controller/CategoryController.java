af94bd480757bd77b8e8760219b511da5cd60bd1
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-03-26 14:28:04 +0530

    Created the CRUD operation environment inside of the categoryController class

--- src/main/java/edu/kolitha/solo/eCom/store/controller/CategoryController.java
+++ src/main/java/edu/kolitha/solo/eCom/store/controller/CategoryController.java
@@ -1,5 +1,30 @@
 package edu.kolitha.solo.eCom.store.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryController {
-    
+    private List<String> categoryList = new ArrayList<>();
+
+    @GetMapping("/shop/category/all")
+    public List<String> getCategoryList(){
+        return categoryList;
+    }
+
+    @PostMapping("/shop/category/add")
+    public String addCategory(@RequestBody String categoryName){
+        return "Category added successfully!";
+    }
+
+    @PutMapping("/shop/category/update")
+    public String updateCategory(@RequestBody String categoryName){
+        return "Category updated!";
+    }
+
+    @DeleteMapping("/shop/category/{id}")
+    public String removeCategory(@PathVariable Long id){
+        return "Category removed!";
+    }
 }
