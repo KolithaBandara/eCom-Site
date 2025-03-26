f17123c01b356e4cda3fbb49d120be4560e16712
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-03-26 22:56:49 +0530

    Decorated the Category class using lombok, @Data and @AllArgsConstructor annotations to short the code

--- src/main/java/edu/kolitha/solo/eCom/store/model/Category.java
+++ src/main/java/edu/kolitha/solo/eCom/store/model/Category.java
@@ -1,27 +1,11 @@
 package edu.kolitha.solo.eCom.store.model;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+
 public class Category {
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
